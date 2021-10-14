3e5582467234cc996e66b27e9f18de2111e79e35
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-10-14 13:25:13 +0000

    second round of tests completed

--- scripts/game.js
+++ scripts/game.js
@@ -5,5 +5,17 @@ let game = {
     choices: ["button1", "button2", "button3", "button4"]
 };
 
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+    showScore();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+// Any new fucntion created needs to be added her from the js file
 // This exports the game script to the game.text.js file
-module.exports = { game };
+module.exports = { game, newGame };
