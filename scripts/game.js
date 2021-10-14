22e38f3a08fb9181ac3ffb8082f51035b823784b
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-10-14 10:34:07 +0000

    initial test code written

---
+++ scripts/game.js
@@ -0,0 +1,9 @@
+let game = {
+    currentGame: [],
+    playerMoves: [],
+    score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
+};
+
+// This exports the game script to the game.text.js file
+module.exports = { game };
